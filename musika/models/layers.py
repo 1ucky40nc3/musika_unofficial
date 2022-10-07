790216ff7d838a4d7e7cffa460c1bbd250e03c47
1ucky40nc3 <louisnwendler@gmail.com>
2022-10-08 00:22:54 +0200

    added decoder implementation

--- musika/models/layers.py
+++ musika/models/layers.py
@@ -48,13 +48,9 @@ def conv_util(
     layers = []
 
     if upsample:
-        layers.append(
-            nn.ConvTranspose2d(**conv_kwargs(**locals()))
-        )
+        layers.append(nn.ConvTranspose2d(**conv_kwargs(**locals())))
     else:
-        layers.append(
-            nn.Conv2d(**conv_kwargs(**locals()))
-        )
+        layers.append(nn.Conv2d(**conv_kwargs(**locals())))
 
     if noise:
         layers.append(nn.Dropout(p=0.1))
@@ -73,7 +69,7 @@ class Encoder2(nn.Module):
         shape: int,
         dim: int = 128,
     ) -> None:
-        kwargs = dict(strides=(1, 1), padding="valid")
+        kwargs = dict(stride=(1, 1), padding="valid")
         self.g0 = conv_util(dim      , 256      , kernel_size=(1, 1), **kwargs)
         self.g1 = conv_util(256      , 256 + 256, kernel_size=(1, 3), **kwargs)
         self.g2 = conv_util(256 + 256, 512 + 128, kernel_size=(1, 3), **kwargs)
@@ -85,7 +81,7 @@ class Encoder2(nn.Module):
             in_channels=512 + 256, 
             out_channels=64, 
             kernel_size=(1, 1), 
-            strides=1,
+            stride=1,
             name="cbottle")
         
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -118,11 +114,11 @@ class Decoder2(nn.Module):
         bottledim
     ) -> None:
         kwargs = dict(kernel_size=(1, 4), noise=True)
-        self.g4 = conv_util(bottledim      , 512 + 128 + 128, strides=(1, 1), upsample=False, **kwargs)
-        self.g3 = conv_util(512 + 128 + 128, 512 + 128 + 128, strides=(1, 2), upsample=True , **kwargs)
-        self.g2 = conv_util(512 + 128 + 128, 512 + 128      , strides=(1, 2), upsample=True , **kwargs)
-        self.g1 = conv_util(512 + 128      , 512            , strides=(1, 1), upsample=False, **kwargs)
-        self.g0 = conv_util(512            , 256 + 128      , strides=(1, 2), upsample=True , **kwargs)
+        self.g4 = conv_util(bottledim      , 512 + 128 + 128, stride=(1, 1), upsample=False, **kwargs)
+        self.g3 = conv_util(512 + 128 + 128, 512 + 128 + 128, stride=(1, 2), upsample=True , **kwargs)
+        self.g2 = conv_util(512 + 128 + 128, 512 + 128      , stride=(1, 2), upsample=True , **kwargs)
+        self.g1 = conv_util(512 + 128      , 512            , stride=(1, 1), upsample=False, **kwargs)
+        self.g0 = conv_util(512            , 256 + 128      , stride=(1, 2), upsample=True , **kwargs)
 
         self.g = nn.Conv2d(
             in_channels=256 + 128, 
@@ -155,7 +151,7 @@ class Encoder(nn.Module):
     ) -> None:
         dim = ((4 * hop) // 2) + 1
 
-        kwargs = dict(kernel_size=(1, 1), strides=(1, 1), padding="valid")
+        kwargs = dict(kernel_size=(1, 1), stride=(1, 1), padding="valid")
         self.g0 = conv_util(dim               , hop * 2 + 32       , **kwargs)
         self.g1 = conv_util(hop * 2 + 32      , hop * 2 + 64       , **kwargs)
         self.g2 = conv_util(hop * 2 + 64      , hop * 2 + 64 + 64  , **kwargs)
@@ -184,3 +180,60 @@ class Encoder(nn.Module):
         x = torch.concat(x, 1)
 
         return x.float()
+
+
+class Decoder(nn.Module):
+    def __init__(
+        self,
+        hop,
+        shape,
+        window
+    ) -> None:
+        self.shape = shape
+        self.window = window
+        dim = ((4 * hop) // 2) + 1
+        
+        kwargs = dict(noise=True)
+        self.g0 = conv_util(128           , hop * 3       , kernel_size=(1, 1), stride=(1, 1), **kwargs)
+        self.g1 = conv_util(hop * 3       , hop * 2       , kernel_size=(1, 3), stride=(1, 2), **kwargs)
+        self.g2 = conv_util(hop * 2       , hop + hop // 2, kernel_size=(1, 3), stride=(1, 2), **kwargs)
+        self.g3 = conv_util(hop + hop // 2, hop + hop // 4, kernel_size=(1, 3), strides=(1, 2), **kwargs)
+        self.g4 = conv_util(hop + hop // 4, hop + hop // 2, kernel_size=(1, 4), stride=(1, 2), upsample=True, **kwargs)
+        self.g5 = conv_util(hop + hop // 2, hop * 2       , kernel_size=(1, 4), stride=(1, 2), upsample=True, **kwargs)
+        self.g6 = conv_util(hop * 2       , hop * 3       , kernel_size=(1, 4), stride=(1, 2), upsample=True, **kwargs)
+        self.g7 = conv_util(hop * 3       , hop * 5       , kernel_size=(1, 1), stride=(1, 1), **kwargs)
+
+        self.g = nn.Conv2d(
+            in_channels=hop * 5,
+            out_channels=dim * 2,
+            kernel_size=(1, 1),
+            strides=(1, 1),
+            padding="same"
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x_g0 = self.g0(x)
+        x_g1 = self.g1(x_g0)
+        x_g2 = self.g2(x_g1)
+        x = self.g3(x_g2)
+        x = self.g4(x)
+        x = self.g5(x + x_g2)
+        x = self.g6(x + x_g1)
+        x = self.g7(x + x_g0)
+
+        x = self.g(x)
+        x = torch.clip(x, -1., 1.)
+
+        gf, pf = torch.split(x, x.shape[-1] // 2, -1)
+        gf = torch.split(gf, gf.shape[1] // self.shape // self.window, 1)
+        gf = torch.concat(gf, -2)
+
+        pf = torch.split(pf, pf.shape[1] // self.shape // self.window, 1)
+        pf = torch.concat(pf, -2)
+
+        s = einops.rearrange(gf, "b c h w -> b h w c")
+        p = einops.rearrange(pf, "b c h w -> b h w c")
+        s = torch.squeeze(s).float()
+        p = torch.squeeze(p).float()
+
+        return s, p
